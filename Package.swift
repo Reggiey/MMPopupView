ad26138b4ba54262b311daf258c504691aa85aab
Reggie <reggiey@163.com>
2024-10-29 15:06:59 +0800

    提交

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MMPopupView",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "MMPopupView", targets: ["MMPopupView"]),
+    ],
+    targets: [
+        .target(name: "MMPopupView", path: "Sources", exclude: []),
+    ]
+)
